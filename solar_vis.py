8db66f0a8b22c4a61e7ca42be759c0202495474e
goha2 <goha2@mail.ru>
2016-11-10 18:25:28 +0300

    Вроде пофиксил vis

--- solar_vis.py
+++ solar_vis.py
@@ -83,7 +83,7 @@ def create_planet_image(space, planet):
     x = scale_x(planet.x)
     y = scale_y(planet.y)
     r = planet.R
-    planet.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=planet.color)# FIXME: сделать как у звезды
+    planet.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=planet.color)# FIXME сделать как у звезды
 
 
 def update_system_name(space, system_name):
